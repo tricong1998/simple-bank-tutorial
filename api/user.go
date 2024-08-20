b6c61b4b4f75c7d41226ca8bcb7b20ab44d1ec09
Cole <cong.nguyen@sotatek.com>
2024-08-20 09:08:15 +0700

    feat: handle password with bcrypt

---
+++ api/user.go
@@ -0,0 +1,65 @@
+package api
+
+import (
+	"net/http"
+	"time"
+
+	db "github.com/Sotatek-CongNguyen/simple-bank-practice/db/sqlc"
+	"github.com/Sotatek-CongNguyen/simple-bank-practice/util"
+	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
+)
+
+type createUserRequest struct {
+	Username string `json:"username" binding:"required,alphanum"`
+	Password string `json:"password" binding:"required,min=6"`
+	Email    string `json:"email" binding:"required,email"`
+	FullName string `json:"full_name" binding:"required"`
+}
+
+type createUserResponse struct {
+	Username          string    `json:"username"`
+	Email             string    `json:"email"`
+	FullName          string    `json:"full_name"`
+	PasswordChangedAt time.Time `json:"password_changed_at"`
+	CreatedAt         time.Time `json:"created_at"`
+}
+
+func (server *Server) createUser(ctx *gin.Context) {
+	var req createUserRequest
+	if err := ctx.ShouldBindJSON((&req)); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	hashedPassword, err := util.HashPassword(req.Password)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	arg := db.CreateUserParams{Username: req.Username, Email: req.Email, FullName: req.FullName, HashedPassword: hashedPassword}
+	user, err := server.store.CreateUser(ctx, arg)
+	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "unique_violation":
+				{
+					ctx.JSON(http.StatusForbidden, errorResponse(err))
+					return
+				}
+			}
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	res := createUserResponse{
+		Username:          user.Username,
+		Email:             user.Email,
+		PasswordChangedAt: user.PasswordChangedAt,
+		FullName:          user.FullName,
+		CreatedAt:         user.CreatedAt,
+	}
+
+	ctx.JSON(http.StatusOK, res)
+}
